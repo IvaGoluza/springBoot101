c09920bd9dc469832246c3faccc636fd9d6005a9
Iva Goluža <ivagoluza@fvffc6kxq05d.home>
2024-09-13 23:05:23 +0200

    init beans from diff packages

--- src/main/java/com/luv2code/springboot/demo/learningapp/LearningappApplication.java
+++ src/main/java/com/luv2code/springboot/demo/learningapp/LearningappApplication.java
@@ -3,7 +3,7 @@ package com.luv2code.springboot.demo.learningapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.luv2code.util", "com.luv2code.springboot.demo.learningapp"})
 public class LearningappApplication {
 
 	public static void main(String[] args) {
