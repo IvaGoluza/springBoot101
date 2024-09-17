e72f509e6caf4dba6c6a97a2cc3d2f5c97a32648
Iva Goluža <ivagoluza@fvffc6kxq05d.home>
2024-09-17 10:28:37 +0200

    use jpa entity manager with dao

--- src/main/java/com/luv2code/springboot/demo/learningapp/LearningappApplication.java
+++ src/main/java/com/luv2code/springboot/demo/learningapp/LearningappApplication.java
@@ -1,7 +1,11 @@
 package com.luv2code.springboot.demo.learningapp;
 
+import com.luv2code.springboot.demo.learningapp.dao.StudentDAO;
+import com.luv2code.springboot.demo.learningapp.entity.Student;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class LearningappApplication {
@@ -9,5 +13,28 @@ public class LearningappApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LearningappApplication.class, args);
 	}
+
+	// will be executed after the beans have been loaded
+	@Bean
+	public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
+		return runner -> {
+			createStudent(studentDAO);
+		};
+	}
+
+	private void createStudent(StudentDAO studentDAO) {
+
+		// create Student object
+		System.out.println("Creating student...");
+		Student student = new Student("iva", "goluza", "goluzaiva@gmail.com");
+
+		// save student
+		System.out.println("Saving student...");
+        studentDAO.save(student);
+
+		// print id of the saved student
+		System.out.println("Done! Generated id:" + student.getId());
+	}
+
 
 }
---
+++ src/main/java/com/luv2code/springboot/demo/learningapp/dao/StudentDAO.java
@@ -0,0 +1,9 @@
+package com.luv2code.springboot.demo.learningapp.dao;
+
+import com.luv2code.springboot.demo.learningapp.entity.Student;
+
+public interface StudentDAO {
+
+    void save(Student student);
+
+}
