0e97032489419be80aac093ed9ae5255fef7f01b
Iva Goluža <ivagoluza@fvffc6kxq05d.home>
2024-09-17 12:29:52 +0200

    add crud for student

--- src/main/java/com/luv2code/springboot/demo/learningapp/LearningappApplication.java
+++ src/main/java/com/luv2code/springboot/demo/learningapp/LearningappApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.util.List;
+
 @SpringBootApplication
 public class LearningappApplication {
 
@@ -18,15 +20,15 @@ public class LearningappApplication {
 	@Bean
 	public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
 		return runner -> {
-			createStudent(studentDAO);
+			crudStudent(studentDAO);
 		};
 	}
 
-	private void createStudent(StudentDAO studentDAO) {
+	private void crudStudent(StudentDAO studentDAO) {
 
 		// create Student object
 		System.out.println("Creating student...");
-		Student student = new Student("iva", "goluza", "goluzaiva@gmail.com");
+		Student student = new Student("andre", "flego", "goluzaiva@gmail.com");
 
 		// save student
 		System.out.println("Saving student...");
@@ -34,6 +36,33 @@ public class LearningappApplication {
 
 		// print id of the saved student
 		System.out.println("Done! Generated id:" + student.getId());
+
+		// retrieve student based on pk
+		Student savedStudent = studentDAO.findById(student.getId());
+		System.out.println("Found last saved student: " + savedStudent);
+
+		// update student name
+		savedStudent.setFirstName("Ella");
+		studentDAO.update(savedStudent);
+
+		// retrieve all students from db
+		List<Student> students = studentDAO.findAll();
+		for (Student s : students) {
+			System.out.println("Found student: " + s);
+		}
+
+		// retrieve students by last name
+		List<Student> studentsByLastName = studentDAO.findByLastName("Goluza");
+		for (Student s : studentsByLastName) {
+			System.out.println("Found student by lastname: " + s);
+		}
+
+		//delete student with id == 2
+		//studentDAO.deleteById(2);
+
+		//delete students with lastname == Flego
+		int rowsDeleted = studentDAO.deleteByLastName("flego");
+		System.out.println("Rows deleted: " + rowsDeleted);
 	}
 
 
--- src/main/java/com/luv2code/springboot/demo/learningapp/dao/StudentDAOImpl.java
+++ src/main/java/com/luv2code/springboot/demo/learningapp/dao/StudentDAOImpl.java
@@ -2,9 +2,13 @@ package com.luv2code.springboot.demo.learningapp.dao;
 
 import com.luv2code.springboot.demo.learningapp.entity.Student;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.Query;
+import jakarta.persistence.TypedQuery;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Repository
 public class StudentDAOImpl implements StudentDAO {
@@ -20,6 +24,45 @@ public class StudentDAOImpl implements StudentDAO {
     @Transactional
     public void save(Student student) {
         entityManager.persist(student);
+    }
+
+    @Override
+    public Student findById(int id) {
+        return entityManager.find(Student.class, id);
+    }
+
+    @Override
+    public List<Student> findAll() {
+        TypedQuery<Student> query = entityManager.createQuery("from Student order by lastName desc", Student.class);
+        return query.getResultList();
+    }
+
+    @Override
+    public List<Student> findByLastName(String lastName) {
+        TypedQuery<Student> query = entityManager.createQuery("select s from Student s where lastName=:theData", Student.class);
+        query.setParameter("theData", lastName);
+        return query.getResultList();
+    }
+
+    @Override
+    @Transactional
+    public void update(Student student) {
+        entityManager.merge(student);
+    }
+
+    @Override
+    @Transactional
+    public void deleteById(int id) {
+        Student student = entityManager.find(Student.class, id);
+        entityManager.remove(student);
+    }
+
+    @Override
+    @Transactional
+    public int deleteByLastName(String lastName) {
+        // do not provide Student.class (query result type param) since this query returns rows affected by delete, not list of entities
+        return entityManager.createQuery("delete from Student s where s.lastName=:theData")
+                .setParameter("theData", lastName).executeUpdate();  // returns deleted rows number
     }
 
 }
