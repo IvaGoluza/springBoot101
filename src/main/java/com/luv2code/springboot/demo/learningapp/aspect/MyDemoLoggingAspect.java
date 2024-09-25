fb78993c721faa0cc8c51fb991e05dd6c0ea36a9
IvaGoluza <ig53401@fer.hr>
2024-09-25 11:32:51 +0200

    use pointcut declaration

--- src/main/java/com/luv2code/springboot/demo/learningapp/aspect/MyDemoLoggingAspect.java
+++ src/main/java/com/luv2code/springboot/demo/learningapp/aspect/MyDemoLoggingAspect.java
@@ -2,16 +2,18 @@ package com.luv2code.springboot.demo.learningapp.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class MyDemoLoggingAspect {
 
-    // POINTCUT EXP: run this code before any target-object method whose name starts with "add" and ACCEPTS anything
-    // HAVE TO specify package name so the wildcard only matches within our project package
-    // if not the error is encountered
-    @Before("execution(* com.luv2code..add*(..))")
+
+    @Pointcut("execution(* com.luv2code.springboot.demo.learningapp.dao.*.*(..))")
+    private void forDaoPackage() {}
+
+    @Before("forDaoPackage()")
     public void beforeAddAccountAdvice() {
         System.out.println("=========== Executing Before Advice on addAccount() ==========");
     }
